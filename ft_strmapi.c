16ea761e00b942176665f57c23c3dabb50004b70
samilim <77201910+samilim@users.noreply.github.com>
2021-01-10 00:48:24 +0100

    Update ft_strmapi.c

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -1,14 +1,16 @@
 
 #include "libft.h"
 
-
 char *ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
   unsigned int i;
   char *res;
   
   i = 0;
-  res = (char *)s;
-  while (res[i])
-  {
-  }
+  if (!s || !(res = malloc(sizeof(char) * ft_strlen(s) + 1)))
+    return (NULL);
+  while (res[i++])
+    res[i] =  (*f)(i, s[i]);
+  res[i] = '\0';
+  return (res);
+}
