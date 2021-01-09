330288068b419182c6e9f2213c6984bc40695722
samilim <77201910+samilim@users.noreply.github.com>
2021-01-10 00:34:17 +0100

    Create ft_strmapi.c

---
+++ ft_strmapi.c
@@ -0,0 +1,14 @@
+
+#include "libft.h"
+
+
+char *ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+  unsigned int i;
+  char *res;
+  
+  i = 0;
+  res = (char *)s;
+  while (res[i])
+  {
+  }
