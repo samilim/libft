ee85a89635b61d4975d841cf76427c36aed869a9
samilim <77201910+samilim@users.noreply.github.com>
2021-01-09 21:57:23 +0100

    Update ft_strnstr.c

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -14,19 +14,23 @@
 
 char    *ft_strnstr(const char *str, const char *to_find, size_t n)
 {
-	int botte;
-	int aiguille;
+	size_t botte;
+	size_t aiguille;
+	char	*cstr;
+	char	*cto_find;
 
+	cstr = (char *)str;
+	cto_find = (char *)to_find;
 	botte = 0;
 	if (*to_find == '\0')
-		return (str);
-	while (str[botte] != '\0' && botte < n)
+		return (cstr);
+	while (cstr[botte] != '\0' && botte < n)
 	{
 		aiguille = 0;
-		while (str[botte + aiguille] == to_find[aiguille])
+		while (cstr[botte + aiguille] == cto_find[aiguille])
 		{
-			if (to_find[aiguille + 1] == '\0')
-				return (&str[botte]);
+			if (cto_find[aiguille + 1] == '\0')
+				return (&cstr[botte]);
 			aiguille++;
 		}
 		botte++;
