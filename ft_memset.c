eebf0922cdac7e6a6cb6df54117e6aecb26dfaca
samilim <77201910+samilim@users.noreply.github.com>
2021-01-09 21:30:47 +0100

    Update ft_memset.c

--- ft_memset.c
+++ ft_memset.c
@@ -19,7 +19,7 @@ void    *ft_memset (void *mem, int ch, size_t n)
 
     i = 0;
     c = (unsigned char *)mem;
-    while (c[i++])
+    while (i++ < n)
         c[i] = (unsigned char)ch;
     return(mem);
 }
