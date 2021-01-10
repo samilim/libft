eab13ef60619a71e6cccb1f95abb36158f12c857
samilim <77201910+samilim@users.noreply.github.com>
2021-01-10 03:15:06 +0100

    Update ft_lstnew.c

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -2,5 +2,10 @@
 
 t_list  *ft_lstnew(void *content)
 {
-
+  t_list  *elem;
+  if (!(elem = malloc(sizeof(t_list))))
+    return (NULL);
+  elem -> content = content;
+  elem -> next = NULL;
+  return (elem);
 }
