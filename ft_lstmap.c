96d55559286a20f50bb7efef55021b4721a9339c
samilim <77201910+samilim@users.noreply.github.com>
2021-01-11 04:33:58 +0100

    Update ft_lstmap.c

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -1,3 +1,18 @@
 #include "libft.h"
 
-t_list *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+t_list  *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+  t_list  *new_lst;
+  t_list  *tmp;
+  
+  if (!(new_list = malloc(sizeof(t_list) * ft_lstsize(lst))))
+    return (NULL);
+  while (new_lst)
+  {
+    tmp = next->lst;
+    new_lst = (f)(lst);
+    new_lst = new_list->next;
+    lst = tmp;
+  }
+  return (new_list);
+}
