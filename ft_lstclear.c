d9669073ca4be4842b62eac8b8020e2762d5bced
samilim <77201910+samilim@users.noreply.github.com>
2021-01-11 02:52:30 +0100

    Update ft_lstclear.c

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -2,5 +2,13 @@
 
 void ft_lstclear(t_list **lst, void (*del)(void*))
 {
-
+  t-list  *tmp;
+  
+  tmp = *lst;
+  while (*lst)
+  {
+    tmp = lst->next;
+    ft_lstdelone(lst, (del));
+    lst = tmp;
+  }
 }
