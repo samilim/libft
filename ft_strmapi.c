ad11d0f3545b1707269a978e723b836a2c715f8b
samilim <77201910+samilim@users.noreply.github.com>
2021-01-14 04:15:31 +0100

    Update ft_strmapi.c

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -20,8 +20,13 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 	i = 0;
 	if (!s || !(res = malloc(sizeof(char) * ft_strlen(s) + 1)))
 		return (NULL);
-	while (res[i++])
+	if (!f)
+		return (ft_strdup(s));
+	while (res[i])
+	{
 		res[i] = (*f)(i, s[i]);
-	res[i] = '\0';
+		i++;
+	}
+	//res[i] = '\0';
 	return (res);
 }
